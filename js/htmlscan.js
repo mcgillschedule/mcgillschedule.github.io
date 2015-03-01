e84799d0c5b040b8d59a57f550c0ec5c825909e5
connorwstein <connor.stein@mail.mcgill.ca>
2015-02-28 23:01:08 -0500

    fixed bugs, can fully parse html into an array of McGill Class Objects

--- js/htmlscan.js
+++ js/htmlscan.js
@@ -10,16 +10,30 @@ reader.onload=function(){
   //Class name comes from caption text skipping the scheduled meeting times
   //All other data comes from the dddefault class, however will need to extract the correct data from there
   var data_tables=dummy_element.getElementsByClassName('datadisplaytable');
-  var mcgill_class=new Object();
-  if(data_tables[0].childNodes[0].innerText=="Scheduled Meeting Times"){
-    mcgill_class.times=data_tables[0].childNodes[1].childNodes[0].childNodes[0].innerText; //bug here cannot extract data from table
+  var mcgill_classes=new Array();
+  
+  for(var i=0;i<data_tables.length;i+=2){
+    var mcgill_class=new Object();
+    mcgill_class.class_name=data_tables[i].caption.textContent; 
+    mcgill_class.times=data_tables[i+1].tBodies[0].rows[1].cells[0].textContent;
+    mcgill_class.days=data_tables[i+1].tBodies[0].rows[1].cells[1].textContent;
+    mcgill_class.classroom=data_tables[i+1].tBodies[0].rows[1].cells[2].textContent;
+    mcgill_class.dates=data_tables[i+1].tBodies[0].rows[1].cells[3].textContent;
+    mcgill_class.format=data_tables[i+1].tBodies[0].rows[1].cells[4].textContent;
+    if(i==0){
+      mcgill_classes[i]=mcgill_class;
+    }
+    else{
+      mcgill_classes[i/2]=mcgill_class;
+    }
+  
   }
-  else{
-    mcgill_class.class_name=data_tables[0].childNodes[0].innerText; //this works
+  for(var k=0;k<mcgill_classes.length;k++){
+    for(var key in mcgill_classes[k]){
+      console.log(mcgill_classes[k][key]);
+    }
   }
-  alert(mcgill_class.class_name);
-  alert(mcgill_class.times);
-
+  
 }
 window.onload=function(){
   var fileInput=document.getElementById("fileInput");
