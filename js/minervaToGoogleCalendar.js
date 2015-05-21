2bf1ff1bb0c427295151e8576a4bf2216f7ec164
connorwstein <connor.stein@mail.mcgill.ca>
2015-05-20 21:31:19 -0400

    changed id of file upload

--- js/minervaToGoogleCalendar.js
+++ js/minervaToGoogleCalendar.js
@@ -38,7 +38,7 @@ reader.onload = function() {
 
 }
 window.onload = function() {
-    var fileInput = document.getElementById("fileInput");
+    var fileInput = document.getElementById("htmlUpload");
     fileInput.onchange = function handle(e) {
         var file = fileInput.files[0];
         reader.readAsText(file);
